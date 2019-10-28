06b92caff5d91e519ff7ccaf7efb173f9aaa36d8
valekar <srini@DESKTOP-1FN9MGD.localdomain>
2019-10-28 22:42:29 +0530

    non expo successfully converted

--- screens/auth/LogoutScreen.tsx
+++ screens/auth/LogoutScreen.tsx
@@ -1,26 +1,24 @@
 import React from "react";
-import { Button, StyleSheet } from "react-native";
-import SafeAreaView, { SafeAreaProvider } from "react-native-safe-area-view";
+import { Button, StyleSheet, SafeAreaView } from "react-native";
+//import SafeAreaView, { SafeAreaProvider } from "react-native-safe-area-view";
 import { DrawerNavigatorItems } from "react-navigation-drawer";
 import { LOGIN } from "../../constants/RouteConstants";
 import CustomButton from "../../components/UI/CustomButton";
 import $t from "../../i18n/index";
 const LogoutScreen = props => (
-  <SafeAreaProvider>
-    <SafeAreaView
-      style={styles.container}
-      forceInset={{ top: "always", horizontal: "never" }}
-    >
-      <DrawerNavigatorItems {...props} />
+  <SafeAreaView
+    style={styles.container}
+    forceInset={{ top: "always", horizontal: "never" }}
+  >
+    <DrawerNavigatorItems {...props} />
 
-      <CustomButton
-        onPress={() => {
-          props.navigation.navigate({ routeName: LOGIN });
-        }}
-        title={$t("auth.logOut")}
-      />
-    </SafeAreaView>
-  </SafeAreaProvider>
+    <CustomButton
+      onPress={() => {
+        props.navigation.navigate({ routeName: LOGIN });
+      }}
+      title={$t("auth.logOut")}
+    />
+  </SafeAreaView>
 );
 
 const styles = StyleSheet.create({
